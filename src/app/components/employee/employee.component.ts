47d33ea4d9e9191faaf0fd8826220650130742f0
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-08-08 01:23:01 +0300

    Create services

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from 'src/app/models/employee';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  employees: Employee[] = [];
+  constructor(private http: HttpClient, employeeService: EmployeeService) {}
 
+  ngOnInit(): void {}
 }
