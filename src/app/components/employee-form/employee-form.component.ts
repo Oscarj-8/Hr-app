26133b091b1c0087e2af2c9cb10394fa85c4de15
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-08-09 17:02:55 +0300

    Add some functions

--- src/app/components/employee-form/employee-form.component.ts
+++ src/app/components/employee-form/employee-form.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+// employee-form.component.ts
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { Employee } from 'src/app/models/employee';
 
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
-  styleUrls: ['./employee-form.component.css']
+  styleUrls: ['./employee-form.component.css'],
 })
-export class EmployeeFormComponent {
+export class EmployeeFormComponent implements OnInit {
+  newEmployee: Employee = {
+    id: 0,
+    name: '',
+    email: '',
+    phone: '',
+    department: '',
+  };
 
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {}
+  addEmployee() {
+    this.employeeService.addEmployee(this.newEmployee);
+    this.newEmployee = {
+      id: 0,
+      name: '',
+      email: '',
+      phone: '',
+      department: '',
+    };
+  }
+
+  cancelAdd() {
+    this.newEmployee = {
+      id: 0,
+      name: '',
+      email: '',
+      phone: '',
+      department: '',
+    };
+  }
 }
--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,94 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Employee } from 'src/app/models/employee';
-// import { EmployeeService } from '../../services/employee.service';
-
-// @Component({
-//   selector: 'app-employee',
-//   templateUrl: './employee.component.html',
-//   styleUrls: ['./employee.component.css'],
-//   providers: [EmployeeService],
-// })
-// export class EmployeeComponent implements OnInit {
-//   employees: Employee[] = [];
-
-//   constructor(
-//     private http: HttpClient,
-//     private employeeService: EmployeeService
-//   ) {}
-
-//   ngOnInit(): void {
-//     this.employeeService
-//       .getEmployees()
-//       .subscribe((employee) => (this.employees = employee));
-//   }
-
-//   deleteEmployee(employee: Employee) {
-//     this.employeeService.deleteEmployee(employee).subscribe(() => {
-//       this.employees = this.employees.filter((e) => e.id !== employee.id);
-//     });
-//   }
-
-//   editEmployee(employee: Employee): void {
-//     console.log('clicked');
-//   }
-// }
-
-// import { Component, OnInit } from '@angular/core';
-// import { Employee } from '../../models/employee';
-// import { EmployeeService } from '../../services/employee.service';
-
-// @Component({
-//   selector: 'app-employee',
-//   templateUrl: './employee.component.html',
-//   styleUrls: ['./employee.component.css'], // Adjust the path to your CSS file
-// })
-// export class EmployeeComponent implements OnInit {
-//   employees: Employee[] = [];
-//   selectedEmployee: Employee | null = null;
-
-//   constructor(private employeeService: EmployeeService) {}
-
-//   ngOnInit(): void {
-//     this.loadEmployees();
-//   }
-
-//   loadEmployees(): void {
-//     this.employeeService.getEmployees().subscribe((employees) => {
-//       this.employees = employees;
-//     });
-//   }
-
-//   selectEmployee(employee: Employee): void {
-//     this.selectedEmployee = { ...employee };
-//   }
-
-//   clearSelection(): void {
-//     this.selectedEmployee = null;
-//   }
-
-//   saveEmployee(employee: Employee): void {
-//     if (employee.id) {
-//       this.employeeService.updateEmployee(employee).subscribe(() => {
-//         this.loadEmployees();
-//         this.clearSelection();
-//       });
-//     } else {
-//       this.employeeService.addEmployee(employee).subscribe(() => {
-//         this.loadEmployees();
-//         this.clearSelection();
-//       });
-//     }
-//   }
-
-//   deleteEmployee(id: number): void {
-//     this.employeeService.deleteEmployee(id).subscribe(() => {
-//       this.loadEmployees();
-//       this.clearSelection();
-//     });
-//   }
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../../services/employee.service';
@@ -96,11 +5,12 @@ import { EmployeeService } from '../../services/employee.service';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css'], // Adjust the path to your CSS file
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
   employees: Employee[] = [];
   selectedEmployee: Employee | null = null;
+  showEmployeeForm = false;
 
   constructor(private employeeService: EmployeeService) {}
 
@@ -121,27 +31,6 @@ export class EmployeeComponent implements OnInit {
   clearSelection(): void {
     this.selectedEmployee = null;
   }
-
-  // saveEmployee(employee: Employee): void {
-  //   if (employee.id) {
-  //     this.employeeService
-  //       .updateEmployee(employee)
-  //       .subscribe((updatedEmployee) => {
-  //         const index = this.employees.findIndex(
-  //           (e) => e.id === updatedEmployee.id
-  //         );
-  //         if (index !== -1) {
-  //           this.employees[index] = updatedEmployee;
-  //         }
-  //         this.clearSelection();
-  //       });
-  //   } else {
-  //     this.employeeService.addEmployee(employee).subscribe((newEmployee) => {
-  //       this.employees.push(newEmployee);
-  //       this.clearSelection();
-  //     });
-  //   }
-  // }
 
   saveEmployee(employee: Employee): void {
     if (employee.id) {
@@ -181,5 +70,13 @@ export class EmployeeComponent implements OnInit {
       this.employees = this.employees.filter((e) => e.id !== id);
       this.clearSelection();
     });
+  }
+
+  openEmployeeForm() {
+    this.showEmployeeForm = !this.showEmployeeForm;
+  }
+
+  closeEmployeeForm() {
+    this.showEmployeeForm = !this.showEmployeeForm;
   }
 }
